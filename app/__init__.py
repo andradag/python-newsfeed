007be8c06af1b9080c7663a791da5264d86edb10
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-12-07 22:44:34 +0000

    creating the home view routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from app.routes import home
 
 def create_app(test_config=None):
   # set up app config
@@ -7,9 +8,12 @@ def create_app(test_config=None):
   app.config.from_mapping(
     SECRET_KEY='super_secret_key'
   )
-  
+
   @app.route('/hello')
   def hello():
     return 'hello world'
+
+   # register routes
+    app.register_blueprint(home)
 
   return app
