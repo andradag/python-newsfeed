fb8b9209ad0e6e74c416fd2a736d4d62daaf6368
Andrada Gacichevici <andradagacichevici@gmail.com>
2022-12-10 13:43:37 +0000

    setting up the routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from app.routes import home
+from app.routes import home, dashboard
 
 def create_app(test_config=None):
   # set up app config
@@ -14,6 +14,7 @@ def create_app(test_config=None):
     return 'hello world'
 
    # register routes
-    app.register_blueprint(home)
+  app.register_blueprint(home)
+  app.register_blueprint(dashboard)
 
   return app
